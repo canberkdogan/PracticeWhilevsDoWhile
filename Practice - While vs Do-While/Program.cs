83b477a618cf9992bef4f707b47bf40e3e1ba471
canbe <canbe@CANBERK>
2024-07-22 23:30:00 +0300

    Add project files.

---
+++ Practice - While vs Do-While/Program.cs
@@ -0,0 +1,28 @@
+﻿//Bu pratikte kullanıcının girdiği bir limit değerine kadar 0'dan bir sayaç çalıştırıp artırarak, ekrana " Ben bir Patika'lıyım " yazdırfım. (WHILE)
+
+Console.WriteLine("Lütfen bir limit değeri giriniz");
+
+int limit = Convert.ToInt32(Console.ReadLine());
+
+int deger = 0;
+                                                                  // While'da kontrol döngünün başında yapıldı.
+while (deger <= limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    deger++;
+}
+
+Console.WriteLine("--------------------------------------------------------------");
+
+
+Console.WriteLine("Lütfen bir limit değeri giriniz");
+
+int limit = Convert.ToInt32(Console.ReadLine());
+int sayi = 0;
+                                                                  // do whlie döngüsünde ise konrol işlem sonunda yapılıyor. Yani koşul yanlış olsa bile 'do' her zaman çalışıyor. 
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayi++;
+
+}while( sayi <= limit);
